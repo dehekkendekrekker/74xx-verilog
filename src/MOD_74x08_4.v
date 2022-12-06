40b23a71028aa02e58680ef2859d728cc3da92e7
Your Name <you@example.com>
2022-12-06 18:09:23 -0500

    Made devices optimizable for yosys

--- src/MOD_74x08_4.v
+++ src/MOD_74x08_4.v
@@ -4,6 +4,9 @@ module MOD_74x08_4 (
     input [0:3] B,
     output [0:3] Y);
 
-assign Y = A & B;
+assign Y[0] = A[0] & B[0];
+assign Y[1] = A[1] & B[1];
+assign Y[2] = A[2] & B[2];
+assign Y[3] = A[3] & B[3];
 
 endmodule
